596257df68d10c27b4eaf9d5fc4c986380d351cd
parkkunwoo <slipper2929@naver.com>
2022-10-07 17:26:54 +0900

    pw Encryption

--- src/main/java/org/boksan/controller/MemberController.java
+++ src/main/java/org/boksan/controller/MemberController.java
@@ -1,8 +1,11 @@
 package org.boksan.controller;
 
+//import javax.inject.Inject;
+
 import org.boksan.model.b_empDTO;
 import org.boksan.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,9 +15,17 @@ public class MemberController {
 	@Autowired
 	MemberService mservice;
 	
+	//@Inject
+	BCryptPasswordEncoder pwdEncoder = new BCryptPasswordEncoder();
+	
 	//회원가입요청
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
 	public String joinInsert(b_empDTO edto) {
+		
+		String inputPass = edto.getPassword();
+		String password = pwdEncoder.encode(inputPass);
+		edto.setPassword(password);
+		
 		mservice.MemberInsert(edto);
 		return "index";
 	}
