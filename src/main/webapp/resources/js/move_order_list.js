ea057929888c325de1f2e94a4b2845790feb0453
hoyeon <dbghduswk@naver.com>
2022-11-09 17:16:24 +0900

    hide

--- src/main/webapp/resources/js/move_order_list.js
+++ src/main/webapp/resources/js/move_order_list.js
@@ -7,8 +7,9 @@ $(document).on("click",".move_compare_btn", function(){
 	if($(this).parent().parent().prev().find($(".move_compare")).val() == $(this).parent().parent().prev().prev().find($(".movement_value")).val()){
 		
 		$(this).prev().attr("name","pallet_num")
+		console.log($(".move_compare"))
 		alert("창고번지가 변경되었습니다.")
-		$("#compare_form").submit();
+		//$("#compare_form").submit();
 		
 	} else{
 		alert("일치하지 않습니다.")
