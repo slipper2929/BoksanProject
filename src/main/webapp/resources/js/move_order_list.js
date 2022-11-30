d5c1e2de4a6dfb08f9e189326330813ba925ff08
hoyeon <dbghduswk@naver.com>
2022-11-30 13:54:36 +0900

    real finish

--- src/main/webapp/resources/js/move_order_list.js
+++ src/main/webapp/resources/js/move_order_list.js
@@ -7,6 +7,8 @@ $(document).on("click",".move_compare_btn", function(){
 	if($(this).parent().parent().prev().find($(".move_compare")).val() == $(this).parent().parent().prev().prev().find($(".movement_value")).val()){
 		
 		$(this).prev().attr("name","pallet_num")
+		$(this).parents("tr").find(".house_code").attr("name","house_code")
+		$(this).parents("tr").find(".movement_address").attr("name","movement_address")
 		console.log($(".move_compare"))
 		alert("창고번지가 변경되었습니다.")
 		$("#compare_form").submit();
